f7321e8fa1e995ce9495c171c6630d3277f9d363
nimrosh <nimrodka@mta.ac.il>
2024-09-08 20:16:05 +0300

    Your commit message

--- a.py
+++ a.py
@@ -1,3 +1,14 @@
-import csv
-from collections import defaultdict
 import pandas as pd
+from web3 import Web3
+
+# pd.options.display.max_rows= None
+# pd.options.display.max_columns=None
+# Read the CSV file into a DataFrame
+data_frame = pd.read_csv('a.csv')
+print(data_frame.columns)
+
+df = data_frame[data_frame['Value_IN(ETH)'] > 1]
+
+print(df)
+df.to_csv('filtered_transactions.csv', index=False)
+
